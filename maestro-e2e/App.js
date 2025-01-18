7c43271c30161dac28f6685861c6ff7e07149b6b
Victor Hugo <pavictorhugo@gmail.com>
2025-01-18 10:25:59 -0300

    maestro poc

---
+++ maestro-e2e/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+
+export default function App() {
+  const [taps, setTaps] = useState(0);
+  const [text, setText] = useState("");
+  return (
+    <SafeAreaView>
+      <Button
+        title="Add one"
+        variant="primary"
+        onPress={() => setTaps(taps + 1)}
+      />
+      <Button
+        title="Add ten"
+        testID="add_ten"
+        onPress={() => setTaps(taps + 10)}
+      />
+      <Text>Number of taps: {taps}</Text>
+      <TextInput
+        testID="text_input"
+        placeholder="Change me!"
+        onChangeText={setText}
+      />
+      <Text>You typed: {text}</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

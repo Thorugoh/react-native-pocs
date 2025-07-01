18c52abe75d9ce63dcbb3918ca68c7fdb183a3f3
Victor Hugo <hugo.victoor77@gmail.com>
2025-07-01 07:40:37 -0300

    feat: Init db

--- infinte-scroll-app/App.tsx
+++ infinte-scroll-app/App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Suspense } from "react"
+import { SQLiteProvider } from 'expo-sqlite';
+import { Home } from './src/app/Home';
+import { databaseInit } from './src/database/databaseInit';
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName='myapp.db' onInit={databaseInit} useSuspense>
+        <Home />
+      </SQLiteProvider>
+    </Suspense>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+;
---
+++ infinte-scroll-app/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { ActivityIndicator } from "react-native";
+
+export function Loading(){
+    return(
+        <ActivityIndicator 
+            style={{flex: 1, alignItems: "center", justifyContent: "center"}}
+        />
+    )
+}

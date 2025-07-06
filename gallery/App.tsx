d0b88e356486811e344ec835661048364254c744
Victor Hugo <hugo.victoor77@gmail.com>
2025-07-06 20:16:57 -0300

    Add types

--- gallery/App.tsx
+++ gallery/App.tsx
@@ -1,44 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import { Button, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import * as MediaLibrary from "expo-media-library"
 import { useEffect, useState } from 'react';
+import { Image } from 'expo-image';
 
 export default function App() {
-  const [albums, setAlbums] = useState([]);
+  const [albums, setAlbums] = useState<MediaLibrary.Album[]>([]);
   const [permissionsResponse, requestPermission] = MediaLibrary.usePermissions();
 
   async function getAlbums(){
     if(permissionsResponse?.status !== "granted") {
       await requestPermission();
     }
+    const fetchedAlbums = await MediaLibrary.getAlbumsAsync({
+      includeSmartAlbums: true,
+    });
+    setAlbums(fetchedAlbums);
   }
   return (
     <SafeAreaView style={styles.container}>
       <Button  onPress={getAlbums} title="Get albums"/>
       <ScrollView>
-        {albums && albums.map((album) => <AlbumEntry album={album} />)}
+        {albums && albums.filter(album => album.assetCount > 0).
+          map((album) => <AlbumEntry key={album?.id} album={album} />)}
       </ScrollView>
 
     </SafeAreaView>
   );
 }
 
-const AlbumEntry = ({ album }) => {
-  const [asset, setAssets] = useState<MediaLibrary.Asset[]>([]);
-
+const AlbumEntry: React.FC<{album: MediaLibrary.Album}> = ({ album }) => {
+  const [assets, setAssets] = useState<MediaLibrary.Asset[]>([]);
+  console.log({assets, album});
+  
   useEffect(() => {
     async function getAlbumAssets(){
       const albumAssets = await MediaLibrary.getAssetsAsync({ album })
       setAssets(albumAssets.assets);
     }
+    getAlbumAssets();
   }, [])
 
   return(
-    <View key={album.id} style={{}}>
+    <View key={album.id} style={{flex: 1, height: 200}}>
       <Text>{album.title} - {album.assetCount ?? 'no'} assets</Text>
       <View style={styles.albumAssetsContainer}>
         {assets && assets.map((asset) => (
-          <Image source={{ uri: asset.uri }} width={50} height={50} />
+          <Image source={{ uri: asset.uri }} style={{width: 50, height: 50, flex: 1}}/>
         ))}
       </View>
     </View>
@@ -48,8 +55,12 @@ const AlbumEntry = ({ album }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  albumAssetsContainer: {
+    height: 300,
+    width: 200,
+    flex: 1,
+    flexWrap: "wrap",
+    gap: 6,
+  }
 });

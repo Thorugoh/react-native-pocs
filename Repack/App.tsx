ab6efd9a48032109c89b8e247fb4041a95398fa5
Victor Hugo <hugo.victoor77@gmail.com>
2025-09-04 22:05:53 -0300

    add script manager

--- Repack/App.tsx
+++ Repack/App.tsx
@@ -6,93 +6,41 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { Script, ScriptManager } from '@callstack/repack/client';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
+  if(__DEV__) {
+    return {
+      url: Script.getDevServerURL(scriptId),
+      cache: false,
+    }
+  }
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  return {
+    url: Script.getRemoteURL(`https://mycdn.example/assets/${scriptId}`)
+  }
+})
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const TeacherModule = React.lazy(() => import('./TeacherModule'));
+const StudentModule = React.lazy(() => import('./StudentModule'));
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+function App({ role }: {role: string}): React.JSX.Element {
+  if(role === "teacher") {
+    return (
+      <React.Suspense fallback={<Text>Loading...</Text>}>
+        <TeacherModule />
+      </React.Suspense>
+    );
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <React.Suspense fallback={<Text>Loading...</Text>}>
+      <StudentModule />
+    </React.Suspense>
   );
 }
 

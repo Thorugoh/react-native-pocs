d69d956184cabe1d5a473a629960811b19a112d7
Victor Hugo <hugo.victoor77@gmail.com>
2025-09-23 06:55:48 -0700

    create simple wbpack config

---
+++ webpack/dist/main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+/******/ })()
+;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-  mode: "none",
+  mode: "none", // "production" | "development" | "none"
   entry: "./src/script.js",
   output: {
     filename: "main.js",

9e4ac84bfde2064a5c57277048a0f10cf67d1224
Victor Hugo <hugo.victoor77@gmail.com>
2025-09-23 07:18:03 -0700

    optmization applied

--- webpack/src/script.js
+++ webpack/src/script.js
@@ -1,9 +1,9 @@
-import _ from 'lodash';
+import join from 'lodash/join.js';
 
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = join(['Hello', 'webpack'], ' ');
 
   return element;
 }

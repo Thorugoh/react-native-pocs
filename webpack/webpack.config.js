e1f4cec828eaf7a13fe89ca89cd0cebd3a75a3c6
Victor Hugo <hugo.victoor77@gmail.com>
2025-09-23 07:10:48 -0700

    optimize bundle

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,22 @@
 
-const path = require("path");
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { fileURLToPath } from "url";
 
-module.exports = {
-  mode: "none", // "production" | "development" | "none"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "production", // "production" | "development" | "none"
   entry: "./src/script.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
 };

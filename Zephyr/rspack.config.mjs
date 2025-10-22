079f3ac437acbc84a5b56c720da53a1acdb32d93
Victor Hugo <hugo.victoor77@gmail.com>
2025-10-22 09:24:39 -0300

    integrate zephyr

--- Zephyr/rspack.config.mjs
+++ Zephyr/rspack.config.mjs
@@ -1,7 +1,7 @@
 import path from 'node:path';
+import { withZephyr } from "zephyr-repack-plugin";
 import { fileURLToPath } from 'node:url';
 import * as Repack from '@callstack/repack';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,25 +12,24 @@ const __dirname = path.dirname(__filename);
  * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
  */
 
-export default Repack.defineRspackConfig({
-  context: __dirname,
-  entry: './index.js',
-  resolve: {
-    ...Repack.getResolveOptions(),
-  },
-  module: {
-    rules: [
-      {
+export default withZephyr()((env) => {
+  return {
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      ...Repack.getResolveOptions()
+    },
+    module: {
+      rules: [{
         test: /\.[cm]?[jt]sx?$/,
         type: 'javascript/auto',
         use: {
           loader: '@callstack/repack/babel-swc-loader',
           parallel: true,
-          options: {},
-        },
-      },
-      ...Repack.getAssetTransformRules(),
-    ],
-  },
-  plugins: [new Repack.RepackPlugin()],
-});
+          options: {}
+        }
+      }, ...Repack.getAssetTransformRules()]
+    },
+    plugins: [new Repack.RepackPlugin()]
+  };
+})

9c295c0411ee4b1463bf2cfd97b3c2d5fd928efd
Victor Hugo <hugo.victoor77@gmail.com>
2025-11-08 08:00:04 -0300

    create app component

--- TurboModuleExample/App.tsx
+++ TurboModuleExample/App.tsx
@@ -1,45 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import NativeLocalStorage from "./specs/NativeLocalStorage";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, StyleSheet, Text, TextInput} from "react-native";
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const EMPTY = "<empty>";
+
+export default function App(): React.JSX.Element {
+  const [value, setValue] = React.useState<string | null>(null);
+
+  const [editingValue, setEditingValue] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    const storedValue = NativeLocalStorage?.getItem("myKey");
+    setValue(storedValue);
+  }, []);
+
+  function saveValue() {
+    NativeLocalStorage?.setItem(editingValue ?? EMPTY, 'myKey');
+    setValue(editingValue);
+  }
+
+  function clearAll() {
+    NativeLocalStorage?.clear();
+    setValue('');
+  }
+
+  function deleteValue() {
+    NativeLocalStorage?.removeItem('myKey');
+    setValue('');
+  }
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text style={styles.text}>
+        Current stored value is: {value ?? "No value"}
+      </Text>
+      <TextInput
+        placeholder="Enter the text you want to store"
+        onChangeText={setEditingValue}
+        style={styles.textInput}  
       />
-    </View>
-  );
+      <Button title="Save" onPress={saveValue} />
+      <Button title="Delete" onPress={deleteValue} />
+      <Button title="Clear" onPress={clearAll} />
+    </SafeAreaView>
+  )
 }
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  text: {
+    margin: 10,
+    fontSize: 20,
+  },
+  textInput: {
+    margin: 10,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 5,
   },
 });
-
-export default App;
